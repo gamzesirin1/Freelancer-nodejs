b4d928a745ae3ee22f8c483fbca25cf20abbceae
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-18 12:49:57 +0300

    partials folder created.

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
 	res.status(200).render('index')
 })
-
+app.get('/add', (req, res) => {
+	res.status(200).render('add')
+})
+app.get('/edit', (req, res) => {
+	res.status(200).render('edit')
+})
 const port = 3000
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
