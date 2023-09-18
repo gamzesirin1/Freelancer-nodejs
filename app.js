f57bdb8cd7400ed15d1ad5a76b1800aad5ea8114
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-18 20:46:45 +0300

    edit project

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
-const esj = require('ejs')
-const app = express()
+const esj = require('ejs');
+const app = express();
 const dotenv = require('dotenv');
 const db = require('./config/db');
 
@@ -13,30 +13,30 @@ dotenv.config();
 
 // Middleware
 const myLogger = (req, res, next) => {
-	console.log('Middleware Log 1')
-	next()
-}
+  console.log('Middleware Log 1');
+  next();
+};
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method', {
-  methods: ['POST', 'GET'],
-}));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.use(myLogger)
+app.use(myLogger);
 // Routes
 app.get('/', photoContollers.getAllPhotos);
 app.get('/photos/:id', photoContollers.getPhoto);
 app.post('/photos', photoContollers.createPhoto);
 app.put('/photos/:id', photoContollers.updatePhoto);
 app.get('/photos/delete/:id', photoContollers.deletePhoto);
-// app.get('/photos/edit/:id', pageControllers.getEditPage);
-// app.get('/',pageContollers.getIndexPage)
-app.get('/add', pageContollers.getAddPage)
-
+app.get('/photos/edit/:id', pageContollers.getEditPage);
+app.get('/add', pageContollers.getAddPage);
 
 const PORT = process.env.PORT || 5000;
 
--- controllers/PhotoControllers.js
+++ controllers/PhotoControllers.js
@@ -1,56 +1,56 @@
-const Photo = require('../models/Photo');
-const fs = require('fs');
+const Photo = require('../models/Photo')
+const fs = require('fs')
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
-};
+	const photos = await Photo.find({}).sort('-dateCreated')
+	res.render('index', {
+		photos
+	})
+}
 
 exports.getPhoto = async (req, res) => {
- 
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-};
+	const photo = await Photo.findById(req.params.id)
+	res.render('photo', {
+		photo
+	})
+}
 
 exports.createPhoto = async (req, res) => {
-  // console.log(req.files.image);
-  // async - await yapısı kullanacğız.
-  // await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
-  // res.redirect('/');
-  const uploadDir = 'public/uploads';
+	// console.log(req.files.image);
+	// async - await yapısı kullanacğız.
+	// await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+	// res.redirect('/');
+	const uploadDir = 'public/uploads'
 
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
+	if (!fs.existsSync(uploadDir)) {
+		fs.mkdirSync(uploadDir)
+	}
 
-  let uploadImage = req.files.image;
-  let uploadPath = __dirname + '/../public/uploads/' + uploadImage.name;
+	let uploadImage = req.files.image
+	let uploadPath = __dirname + '/../public/uploads/' + uploadImage.name
 
-  uploadImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadImage.name,
-    });
-    res.redirect('/');
-  });
-};
+	uploadImage.mv(uploadPath, async () => {
+		await Photo.create({
+			...req.body,
+			image: '/uploads/' + uploadImage.name
+		})
+		res.redirect('/')
+	})
+}
 
 exports.updatePhoto = async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-  res.redirect(`/photos/${req.params.id}`);
-};
+	const photo = await Photo.findOne({ _id: req.params.id })
+	photo.title = req.body.title
+	photo.description = req.body.description
+	photo.save()
+
+	res.redirect('/')
+}
 
 exports.deletePhoto = async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + '/../public' + photo.image;
-  fs.unlinkSync(deletedImage);
-  await Photo.findByIdAndRemove(req.params.id);
-  res.redirect('/');
-};
+	const photo = await Photo.findOne({ _id: req.params.id })
+	let deletedImage = __dirname + '/../public' + photo.image
+	fs.unlinkSync(deletedImage)
+	await Photo.findByIdAndRemove(req.params.id)
+	res.redirect('/')
+}
